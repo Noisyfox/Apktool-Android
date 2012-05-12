6c75c4a08d58af185d19b4617b7e536548ec9b03
Noisyfox <timemanager.rick@gmail.com>
2012-05-12 22:15:58 +0800

    Format code.

--- src/per/pqy/apktool/About.java
+++ src/per/pqy/apktool/About.java
@@ -4,20 +4,11 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class About extends Activity {
-	
+
 	@Override
-	
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.about);
-        
-        
-        
-            
-            
-        
-        }}
-	
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.about);
 
-
-
+	}
+}
--- src/per/pqy/apktool/SystemManager.java
+++ src/per/pqy/apktool/SystemManager.java
@@ -3,30 +3,32 @@ package per.pqy.apktool;
 import android.app.Activity;
 import java.io.DataOutputStream;
 import android.util.Log;
+
 public class SystemManager extends Activity {
-	
-	
-		public static boolean RootCommand(String command)    {        
-			Process process = null;        
-			DataOutputStream os = null;
-			try{            
-				process = Runtime.getRuntime().exec("su");           
-				os = new DataOutputStream(process.getOutputStream());           
-				os.writeBytes(command +"\n");           
-				os.writeBytes("exit\n");           
-				os.flush();           
-				process.waitFor();       
-				} catch (Exception e){            
-					Log.d("*** DEBUG ***","ROOT REE" + e.getMessage());
-					return false;        } 
-			finally{
-				try{
-						if (os != null){  os.close();  }                
-						process.destroy();            } catch (Exception e)            {            }
-				}       
-			Log.d("*** DEBUG ***", "RootSUC ");
-			return true;    
+
+	public static boolean RootCommand(String command) {
+		Process process = null;
+		DataOutputStream os = null;
+		try {
+			process = Runtime.getRuntime().exec("su");
+			os = new DataOutputStream(process.getOutputStream());
+			os.writeBytes(command + "\n");
+			os.writeBytes("exit\n");
+			os.flush();
+			process.waitFor();
+		} catch (Exception e) {
+			Log.d("*** DEBUG ***", "ROOT REE" + e.getMessage());
+			return false;
+		} finally {
+			try {
+				if (os != null) {
+					os.close();
+				}
+				process.destroy();
+			} catch (Exception e) {
 			}
 		}
-
-
+		Log.d("*** DEBUG ***", "RootSUC ");
+		return true;
+	}
+}
